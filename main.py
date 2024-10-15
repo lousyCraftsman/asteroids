68705f393249c514348d8b559baa24abfcc8ff47
tragic-cmd <codyshouey@gmail.com>
2024-10-14 21:06:01 -0400

    C: Restricted framerate in main.py

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
-    while True: 
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
